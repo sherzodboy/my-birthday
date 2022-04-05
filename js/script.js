59e50d2df85559ca543806326d0ca68bed64472b
sherzod02 <kholmamatovsh09>
2022-04-05 05:07:36 +0500

    my birthday is left

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const currentYear = new Date().getFullYear();
+
+const myBirthday = new Date(`September 9 ${currentYear} 00:00:00`);
+
+function updateCountdowntime() {
+  const currentTime = new Date();
+  const diff = myBirthday - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? "0" + h : h;
+  minutes.innerHTML = m < 10 ? "0" + m : m;
+  seconds.innerHTML = s < 10 ? "0" + s : s;
+}
+
+setInterval(updateCountdowntime, 1000);
